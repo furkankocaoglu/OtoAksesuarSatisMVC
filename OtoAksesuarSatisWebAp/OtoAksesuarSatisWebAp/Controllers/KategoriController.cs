e770f04449d3487b6559ec5de655ed5ebe5668bb
lincarsia <kocaoglufurkan818@gmail.com>
2025-05-07 06:45:08 +0300

    Ana sayfa kategori listeleme yapıldı

---
+++ OtoAksesuarSatisWebAp/OtoAksesuarSatisWebAp/Controllers/KategoriController.cs
@@ -0,0 +1,30 @@
+﻿using OtoAksesuarSatisWebAp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtoAksesuarSatisWebAp.Controllers
+{
+    public class KategoriController : Controller
+    {
+        OtoAksesuarSatisDB db= new OtoAksesuarSatisDB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategories()
+        {
+            var kategoriler = db.Kategoriler.Where(k => k.Silinmis == false).ToList(); // 
+            return PartialView(kategoriler); 
+        }
+        public ActionResult Urunler(int kategoriId)
+        {
+            var urunler = db.Urunler
+                     .Where(u => u.KategoriID == kategoriId && u.Silinmis == false)
+                     .ToList();
+            return View(urunler);
+        }
+    }
+}
